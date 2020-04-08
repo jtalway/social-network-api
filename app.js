9312d20fae8729599d602f764e6304b5b8cd927b
Jason Alway <jtalway@gmail.com>
2020-04-08 13:56:04 -0500

    added api to routes

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 // apiDocs
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   fs.readFile("docs/apiDocs.json", (err, data) => {
     if(err) {
       res.status(400).json({
@@ -60,9 +60,9 @@ app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
 // handle any requests coming to base url
-app.use("/", postRoutes);
-app.use("/", authRoutes);
-app.use("/", userRoutes);
+app.use("/api", postRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 app.use(function (err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     res.status(401).json({error: "Unauthorized Access."});
